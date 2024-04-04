6e08d450473a6d05aa361d7d93d239d3eb81a91a
RazerGhost <90572937+RazerGhost@users.noreply.github.com>
2024-04-04 14:53:41 +0200

    1/2

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,81 +8,75 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                <form method="POST" action="{{ route('admin.storeemployee') }}">
+                <form method="POST" action="{{ route('admin.storeuser') }}" class="p-6">
                     @csrf
 
                     <!-- First Name -->
                     <div class="mb-4">
-                        <x-input-label for="first_name" :value="__('Name')" />
-                        <input id="first_name" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" type="text" name="first_name"
-                            :value="old('first_name')" required autofocus autocomplete="first_name" />
+                        <x-input-label for="first_name" :value="__('First Name')" />
+                        <input id="first_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" />
                         <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
                     </div>
 
                     <!-- Last Name -->
                     <div class="mb-4">
                         <x-input-label for="last_name" :value="__('Last Name')" />
-                        <input id="last_name" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" type="text" name="last_name"
-                            :value="old('last_name')" required autocomplete="last_name" />
+                        <input id="last_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="last_name" :value="old('last_name')" required autocomplete="last_name" />
                         <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
                     </div>
 
-
+                    <!-- Tel Number -->
                     <div class="mb-4">
-                        <label for="tel_number" class="mb-2 block text-sm text-gray-600 dark:text-gray-400">Tel Number</label>
-                        <input type="tel" name="tel_number" id="tel_number" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" required>
+                        <label for="tel_number" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Tel Number') }}</label>
+                        <input type="tel" name="tel_number" id="tel_number" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required>
                     </div>
 
                     <!-- Email Address -->
                     <div class="mb-4">
                         <x-input-label for="email" :value="__('Email')" />
-                        <input id="email" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" type="email" name="email"
-                            :value="old('email')" required autocomplete="username" />
+                        <input id="email" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="email" name="email" :value="old('email')" required autocomplete="username" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <!-- Password -->
                     <div class="mb-4">
                         <x-input-label for="password" :value="__('Password')" />
-
-                        <input id="password" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" type="password" name="password" required
-                            autocomplete="new-password" />
-
+                        <input id="password" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="password" name="password" required autocomplete="new-password" />
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
 
                     <!-- Confirm Password -->
                     <div class="mb-4">
                         <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                        <input id="password_confirmation" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+                        <input id="password_confirmation" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="password" name="password_confirmation" required autocomplete="new-password" />
                         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     </div>
 
-                    {{-- address --}}
+                    <!-- Address -->
                     <div class="mb-4">
-                        <label for="address" class="mb-2 block text-sm text-gray-600 dark:text-gray-400">Address</label>
-                        <input type="text" name="address" id="address" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" required>
-                    </div>
-                    {{-- city --}}
-                    <div class="mb-4">
-                        <label for="city" class="mb-2 block text-sm text-gray-600 dark:text-gray-400">City</label>
-                        <input type="text" name="city" id="city" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" required>
-                    </div>
-                    {{-- state --}}
-                    <div class="mb-4">
-                        <label for="state" class="mb-2 block text-sm text-gray-600 dark:text-gray-400">State</label>
-                        <input type="text" name="state" id="state" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" required>
-                    </div>
-                    {{-- zip --}}
-                    <div class="mb-4">
-                        <label for="zip" class="mb-2 block text-sm text-gray-600 dark:text-gray-400">Zip</label>
-                        <input type="text" name="zip" id="zip" class="w-full rounded-md border border-gray-300 p-2 dark:bg-gray-700 dark:text-gray-300" required>
+                        <label for="address" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Address') }}</label>
+                        <input type="text" name="address" id="address" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required>
                     </div>
 
+                    <!-- City -->
+                    <div class="mb-4">
+                        <label for="city" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('City') }}</label>
+                        <input type="text" name="city" id="city" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required>
+                    </div>
 
+                    <!-- State -->
+                    <div class="mb-4">
+                        <label for="state" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('State') }}</label>
+                        <input type="text" name="state" id="state" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required>
+                    </div>
+
+                    <!-- Zip -->
+                    <div class="mb-4">
+                        <label for="zip" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Zip') }}</label>
+                        <input type="text" name="zip" id="zip" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required>
+                    </div>
+
+                    <!-- Role -->
                     <x-text-input class="hidden" type="text" name="role" id="role" value="1" />
 
                     <div class="flex items-center justify-end mb-4">
