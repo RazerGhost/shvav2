298cd0de4dc1951d4481bb8527f08a1e35349755
RazerGhost <90572937+RazerGhost@users.noreply.github.com>
2024-04-04 16:17:14 +0200

    partially fixed edit page Getting Error ({"message":"Undefined constant \"user\""})

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -11,21 +11,32 @@
                 <form method="POST" action="{{ route('admin.updateuser', $user) }}" class="p-6">
                     @csrf
                     @method('PATCH')
-                    <div class="grid grid-cols-2 gap-4 mb-4">
-                        <!-- First Name -->
-                        <div>
-                            <x-input-label for="first_name" :value="__('First Name')" />
-                            <input id="first_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" value="{{ $user->first_name }}" />
-                            <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                    @if ( $user->role == 3)
+                        <div class="grid grid-cols-2 gap-4 mb-4">
+                            <!-- Name -->
+                            <div>
+                                <x-input-label for="first_name" :value="__('Naam')" />
+                                <input id="first_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" value="{{ $user->first_name }}" />
+                                <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                            </div>
                         </div>
+                    @else
+                        <div class="grid grid-cols-2 gap-4 mb-4">
+                            <!-- First Name -->
+                            <div>
+                                <x-input-label for="first_name" :value="__('Voornaam')" />
+                                <input id="first_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" value="{{ $user->first_name }}" />
+                                <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+                            </div>
 
-                        <!-- Last Name -->
-                        <div>
-                            <x-input-label for="last_name" :value="__('Last Name')" />
-                            <input id="last_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="last_name" :value="old('last_name')" required autocomplete="last_name" value="{{ $user->last_name }}" />
-                            <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                            <!-- Last Name -->
+                            <div>
+                                <x-input-label for="last_name" :value="__('Achternaam')" />
+                                <input id="last_name" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="text" name="last_name" :value="old('last_name')" required autocomplete="last_name" value="{{ $user->last_name }}" />
+                                <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                            </div>
                         </div>
-                    </div>
+                    @endif
 
                     <!-- Tel Number -->
                     <div class="mb-4">
@@ -36,15 +47,15 @@
                     <!-- Email Address -->
                     <div class="mb-4">
                         <x-input-label for="email" :value="__('Email')" />
-                        <input id="email" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="email" name="email" :value="old('email')" required autocomplete="username" value="{{ $user->email }}"/>
+                        <input id="email" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="email" name="email" :value="old('email')" required autocomplete="username" value="{{ $user->email }}" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <!-- Password -->
                     <div class="grid grid-cols-2 gap-4 mb-4">
                         <div>
-                            <x-input-label for="password" :value="__('Password')" />
-                            <input id="password" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="password" name="password" required autocomplete="new-password" value="{{ $user->password }}"/>
+                            <x-input-label for="password" :value="__('Wachtwoord')" />
+                            <input id="password" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" type="password" name="password" required autocomplete="new-password" value="{{ $user->password }}" />
                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
 
@@ -52,7 +63,7 @@
 
                     <!-- Address -->
                     <div class="mb-4">
-                        <label for="address" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Address') }}</label>
+                        <label for="address" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Adres') }}</label>
                         <input type="text" name="address" id="address" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required value="{{ $user->address }}">
                     </div>
 
@@ -60,25 +71,25 @@
                     <div class="grid grid-cols-3 gap-4 mb-4">
                         <!-- City -->
                         <div>
-                            <label for="city" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('City') }}</label>
+                            <label for="city" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Stad') }}</label>
                             <input type="text" name="city" id="city" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required value="{{ $user->city }}">
                         </div>
 
                         <!-- State -->
                         <div>
-                            <label for="state" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('State') }}</label>
+                            <label for="state" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Provincie') }}</label>
                             <input type="text" name="state" id="state" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required value="{{ $user->state }}">
                         </div>
 
                         <!-- Zip -->
                         <div>
-                            <label for="zip" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Zip') }}</label>
+                            <label for="zip" class="block text-sm text-gray-600 dark:text-gray-400">{{ __('Postcode') }}</label>
                             <input type="text" name="zip" id="zip" class="w-full p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" required value="{{ $user->zip }}">
                         </div>
                     </div>
 
                     <!-- Role -->
-                    <x-text-input class="hidden" type="text" name="role" id="role" value="{{ request()->query('role') }}" />
+                    <x-text-input class="hidden" type="text" name="role" id="role" value="{{ user->role }}" />
 
                     <div class="flex items-center justify-end mb-4">
                         <x-primary-button class="ms-4">
