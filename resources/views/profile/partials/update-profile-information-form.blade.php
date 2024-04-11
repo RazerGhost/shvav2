66e1d87489627f8b5fc2bf8717d1ce402b17742c
RazerGhost <90572937+RazerGhost@users.noreply.github.com>
2024-04-11 13:21:29 +0200

    fixed notifications

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,4 +1,9 @@
 <section class="mt-6 space-y-6">
+    @if (session('status') === 'profile-updated')
+        <script>
+            showNotification('Info', 'Informatie successvol opgeslagen', 'info');
+        </script>
+    @endif
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Profile Information') }}
@@ -80,10 +85,6 @@
 
         <div class="flex items-center">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="ml-4 text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
-            @endif
         </div>
     </form>
 </section>
